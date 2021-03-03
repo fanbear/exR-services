df0e15bc61a081549a09ca308a611fe4b5ce8693
Yura <fanbearytb@gmail.com>
2021-03-04 00:26:56 +0200

    create express-server, db-connect, mongoose, bable, nodemon

---
+++ dataBase/connect.js
@@ -0,0 +1,4 @@
+
+module.exports = {
+    URI: 'mongodb+srv://exr-services:ueeuWNpdbwW5IzL9@cluster0.ehdxg.mongodb.net/services',
+};
---
+++ index.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+
+const connect = require('./dataBase/connect');
+
+
+
+
+const app = express();
+const port = 3000;
+
+console.log(connect.URI);
+// DataBase connect
+mongoose.connect(connect.URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() =>  console.log("MongoDB connected"))
+    .catch(err => console.log(err))
+
+
+// Routing
+const Routes = require('./routes/index');
+
+// Used module
+app.use(Routes);
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json({
+    extended: true,
+    inflate: true,
+    parameterLimit: 1000,
+    type: 'application/json',
+    verify: undefined
+}));
+
+
+// Server
+app.listen(port, () => {
+    console.log("Express server start");
+})
